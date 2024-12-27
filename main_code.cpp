23a554c0476886ce4dda89f85b551b81dd2f058d
Biniyam Kinfe <biniyamkinfe21@gmail.com>
2024-12-27 09:57:52 -0800

    adding functions to the code

--- main_code.cpp
+++ main_code.cpp
@@ -4,3 +4,31 @@
 
 using namespace std;
 
+void add_product(int id, string name, string catagory, double price, int quantity){
+
+}
+
+void remove_product(int id){
+
+}
+
+void find_product(int id){
+
+}
+
+void update_product(int id, string name, string catagory, double price, int quantity){
+
+}
+
+void print_product(){
+
+}
+
+void saveToFile(string filename){
+
+}
+
+
+int main(){
+    
+}
